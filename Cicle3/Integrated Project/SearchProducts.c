05029e7fe819004168876c955d89d61397392b4b
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-07 18:27:12 -0200

    Beginning Integrated Project EAD

---
+++ Cicle3/Integrated Project/SearchProducts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+    typedef struct{
+        char Name[40];
+        float Price;
+        int Quantity;
+
+    } Product;
+
+main()
+{
+
+}
+
+void OrderProductsByCategory()
+{
+
+}
+
+
