ed26efae619eb3d63ae3b8caff7533de26e9a23e
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-08 06:55:06 -0200

    Continuing EAD PI

--- Cicle3/Integrated Project/SearchProducts.c
+++ Cicle3/Integrated Project/SearchProducts.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
 
     typedef struct{
-        char Name[40];
+        char* Name;
         float Price;
         int Quantity;
 
     } Product;
 
+    Product _products[100];
 main()
 {
 
+}
+
+void InitializeProducts()
+{
+    int i = 0;
+    for(i; i < 10; i++)
+    {
+        _products[i].Name = "Product";
+    }
 }
 
 void OrderProductsByCategory()
