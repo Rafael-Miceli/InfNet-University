b8c7d18daf6562018b37811d11f82e4d275b8aaf
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-12 09:28:24 -0200

    Implementing Linked List in C

---
+++ Cicle3/Data Structures and Algorithms/LinkedListStudy/LinkedList.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+    struct node{
+        int x;
+        struct node *next;
+    };
+
+int main()
+{
+    /*
+
+    //This is the root, the first and unchanging node
+    struct node *root;
+
+    //Now root points to a node struct
+    root = (struct node *) malloc(sizeof(struct node));
+
+    //The node root points has its next pointer equal to a null pointer set
+    root -> next = 0;
+
+    //By using the "->" operator, you can modify what the node, a pointer, (root in this case) points to
+    root -> x = 5;
+
+    */
+
+
+
+}
+
