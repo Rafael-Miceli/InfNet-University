d08a6a2488ea10b9c8bec9fcc94f3e95dcb653bd
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-12 11:34:40 -0200

    Implementing C LinkedList

--- Cicle3/Data Structures and Algorithms/LinkedListStudy/LinkedList.c
+++ Cicle3/Data Structures and Algorithms/LinkedListStudy/LinkedList.c
@@ -1,10 +1,72 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<stdbool.h>
 
     struct node{
-        int x;
+        int val;
         struct node *next;
     };
+
+    struct node *head = NULL;
+    struct node *curr = NULL;
+
+struct node* Create(int val)
+{
+    printf("\n creating list with root node as [%d]\n", val);
+    struct node *ptr = (struct node*)malloc(sizeof(struct node));
+
+    if(ptr == NULL)
+    {
+        printf("\n Node creation failed \n");
+        return NULL;
+    }
+
+    ptr->val = val;
+    ptr->next = NULL;
+
+    head = curr = ptr;
+    return ptr;
+}
+
+struct node* Add(int val, bool addToEnd)
+{
+    if(head == NULL)
+        return (Create(val));
+
+    if(addToEnd)
+        printf("\n Adding node to the end of list with value[%d]\n", val);
+    else
+        printf("\n Adding node to the beginning of list with value[%d]\n", val);
+
+    struct node *ptr = (struct node*)malloc(sizeof(struct node));
+
+    if(ptr == NULL)
+    {
+        printf("\n Node creation failed \n");
+        return NULL;
+    }
+
+    ptr->val = val;
+    ptr->next = NULL;
+
+    if(addToEnd)
+    {
+        curr->next = ptr;
+        curr = ptr;
+    }
+    else
+    {
+        ptr->next = head;
+        head = ptr;
+    }
+
+    return ptr;
+}
+
+struct node* Find(int val, struct node **prev)
+{
+
+}
 
 int main()
 {
@@ -20,7 +82,7 @@ int main()
     root -> next = 0;
 
     //By using the "->" operator, you can modify what the node, a pointer, (root in this case) points to
-    root -> x = 5;
+    root -> val = 5;
 
     */
 
