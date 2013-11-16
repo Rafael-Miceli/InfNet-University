f203ef949779ebef3f99bdbcdc3c8f44805dad8f
Rafael Miceli <rafael.miceli@hotmail.com>
2013-11-16 12:16:44 -0200

    Implementing LinkedList exercise

--- Cicle3/Data Structures and Algorithms/Exercises AV2/LinkedListAddBeginAddEnd.c
+++ Cicle3/Data Structures and Algorithms/Exercises AV2/LinkedListAddBeginAddEnd.c
@@ -3,12 +3,105 @@
 #include<stdbool.h>
 
     struct node{
-        char val[50];
+        char *val;
         struct node *next;
     };
 
     struct node *root = NULL;
     struct node *curr = NULL;
+
+int main()
+{
+    int i = 0, ret = 0, choice = 0;
+    char value[50];
+    struct node *ptr = NULL;
+
+    while(1)
+    {
+        printf("1.Insert\n");
+        printf("2.Delete\n");
+        printf("3.Display all elements of the List\n");
+        printf("4.Exit\n");
+        printf("Enter your choice : ");
+        scanf("%d",&choice);
+
+        switch(choice)
+        {
+             case 1:
+                 printf("1.Add to the beginning List?\n");
+                 printf("2.Add to the end List?\n");
+                 scanf("%d",&choice);
+
+                 switch(choice)
+                 {
+                    case 1:
+                        printf("\nEnter a value to add to the List: ");
+                        scanf("%s", value);
+                        Add(value, false);
+                        break;
+                    case 2:
+                        printf("\nEnter a value to add to the List: ");
+                        scanf("%s", value);
+                        Add(value, true);
+                        break;
+                    default:
+                        printf("Wrong choice\n");
+                 }
+
+
+                 break;
+             case 2:
+                 printf("\nEnter a value to add to the List: ");
+                 scanf("%s", value);
+                 Delete(value);
+                 break;
+             case 3:
+                 PrintList();
+                 break;
+             case 4:
+                 exit(1);
+             default:
+                 printf("Wrong choice\n");
+        }
+    }
+
+    /*
+
+    for(i = 5; i<10; i++)
+        Add(i*2, true);
+
+    PrintList();
+
+    for(i = 4; i>0; i--)
+        Add(i, false);
+
+    PrintList();
+
+    for(i = 1; i<10; i+=4)
+    {
+        ptr = Find(i*2, NULL);
+
+        if(ptr == NULL)
+            printf("\n Search [val = %d] failed, no element found \n", i*2);
+        else
+            printf("\n Search passed [val = %d] \n", ptr->val);
+
+        PrintList();
+
+        ret = Delete(i*2);
+        if(ret != 0)
+            printf("\n Delete [val = %d] failed, no element found \n", i*2);
+        else
+            printf("\n Delete [val = %d] passed \n", i*2);
+
+        PrintList();
+
+        return 0;
+    }
+    PrintList();
+
+    */
+}
 
 struct node* Create(char *val)
 {
@@ -28,7 +121,7 @@ struct node* Create(char *val)
     return ptr;
 }
 
-struct node* Add(int val, bool addToEnd)
+struct node* Add(char *val, bool addToEnd)
 {
     if(root == NULL)
         return (Create(val));
@@ -141,45 +234,3 @@ void PrintList()
 
     return;
 }
-
-int main()
-{
-    int i = 0, ret = 0;
-    struct node *ptr = NULL;
-
-    PrintList();
-
-    for(i = 5; i<10; i++)
-        Add(i*2, true);
-
-    PrintList();
-
-    for(i = 4; i>0; i--)
-        Add(i, false);
-
-    PrintList();
-
-    for(i = 1; i<10; i+=4)
-    {
-        ptr = Find(i*2, NULL);
-
-        if(ptr == NULL)
-            printf("\n Search [val = %d] failed, no element found \n", i*2);
-        else
-            printf("\n Search passed [val = %d] \n", ptr->val);
-
-        PrintList();
-
-        ret = Delete(i*2);
-        if(ret != 0)
-            printf("\n Delete [val = %d] failed, no element found \n", i*2);
-        else
-            printf("\n Delete [val = %d] passed \n", i*2);
-
-        PrintList();
-
-        return 0;
-    }
-    PrintList();
-}
-
