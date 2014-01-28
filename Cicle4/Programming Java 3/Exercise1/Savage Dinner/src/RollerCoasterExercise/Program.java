4f3f40de0f79691887b2c9def3c5969986bb71d8
Rafael Miceli <rafael.miceli@hotmail.com>
2014-01-28 07:19:50 -0200

    New Java exercise

---
+++ Cicle4/Programming Java 3/Exercise1/Savage Dinner/src/RollerCoasterExercise/Program.java
@@ -0,0 +1,15 @@
+package RollerCoasterExercise;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rafael
+ * Date: 27/01/14
+ * Time: 23:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class Program {
+    public static void main(String[] args)
+    {
+
+    }
+}
