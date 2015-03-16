b15c0efeaaaa9d58236394d10cf2d8677b25c5b9
rafael-miceli <rafael.miceli@hotmail.com>
2015-03-16 13:54:12 -0300

    Added Builder Pattern project

---
+++ Cicle8/.Net II/SlnBuilderPattern/SubscribeToWorkshop.Domain/Interfaces/CloudService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubscribeToWorkshop.Domain
+{
+    public interface CloudService<T>
+    {
+        void Add(T entity);
+    }
+}
