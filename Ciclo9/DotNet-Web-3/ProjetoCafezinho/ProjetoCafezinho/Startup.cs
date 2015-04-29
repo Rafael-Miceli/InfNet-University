9a0f18819bf16ce6cee46c94a602f443dd412d13
rafael-miceli <rafael.miceli@hotmail.com>
2015-04-29 07:24:24 -0300

    AspNet project cafezinho

---
+++ Ciclo9/DotNet-Web-3/ProjetoCafezinho/ProjetoCafezinho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetoCafezinho.Startup))]
+namespace ProjetoCafezinho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
